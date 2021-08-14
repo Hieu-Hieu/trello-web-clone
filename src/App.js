f23d07a369901f03c07a96cfef5d642ebc163b30
Hieu Hieu <hieuld007@gmail.com>
2021-08-14 15:46:33 +0700

    Design theme v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,89 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello
-      </h1>
+    <div className="trello">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>I can do it</header>
+
+          <ul>
+            <li>
+              <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/nubelson-fernandes-CO6r5hbt1jg-unsplash.jpg" alt="img" />
+              Title: hieuhieu
+            </li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+          </ul>
+
+          <footer>Add anoter card</footer>
+        </div>
+
+        <div className="column">
+          <header>I can do it</header>
+          <ul>
+            <li>
+              <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/nubelson-fernandes-CO6r5hbt1jg-unsplash.jpg" alt="img" />
+              Title: hieuhieu
+            </li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+          </ul>
+
+          <footer>Add anoter card</footer>
+        </div>
+
+        <div className="column">
+          <header>I can do it</header>
+          <ul>
+            <li>
+              <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/nubelson-fernandes-CO6r5hbt1jg-unsplash.jpg" alt="img" />
+              Title: hieuhieu
+            </li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+          </ul>
+          <footer>Add anoter card</footer>
+        </div>
+
+        <div className="column">
+          <header>I can do it</header>
+          <ul>
+            <li>
+              <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/nubelson-fernandes-CO6r5hbt1jg-unsplash.jpg" alt="img" />
+              Title: hieuhieu
+            </li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+          </ul>
+          <footer>Add anoter card</footer>
+        </div>
+
+        <div className="column">
+          <header>I can do it</header>
+          <ul>
+            <li>
+              <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/nubelson-fernandes-CO6r5hbt1jg-unsplash.jpg" alt="img" />
+              Title: hieuhieu
+            </li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+            <li>Tomorrow will better than today because you can do that</li>
+          </ul>
+          <footer>Add anoter card</footer>
+        </div>
+      </div>
     </div>
   );
 }
